c1829e48b7320e14ca9cab2d6103f0bf09704615
moisesmrp2009 <33405821+moisesmrp2009@users.noreply.github.com>
2019-01-08 14:14:57 -0500

    (feature) Creating a make-believe database
    The database will retain will create files containing the information
    of all the users and will provide information about the users

---
+++ src/database/DatabaseManager.java
@@ -0,0 +1,24 @@
+package database;
+ 
+import laundrytimesheet.Person;
+/**
+ * This class will create files.txt with the personal information of the users
+ * possible operation:
+ * 1 --> add(Person)
+ * 2 --> doesUserExit(Person)
+ * 3 --> removeUser(Person)  
+ * @author LaundryApp
+ */
+public class DatabaseManager {
+	
+	
+	
+	/**
+	 * saves space in disk for a Person object 
+	 * @param person object being added to the database
+	 * @return whether was able to create and save the Person on disk
+	 */
+	public boolean add(Person person) {
+		return true;
+	}
+}
