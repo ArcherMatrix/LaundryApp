b7190485045374993ad6587f0c71fe622b92d2f9
ArcherMatrix <sehbailey455@live.com>
2019-01-09 13:44:17 -0500

    Updated DatabaseManager & Person class

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -1,25 +1,51 @@
 package database;
  
+import java.io.PrintWriter;
+
 import laundrytimesheet.Person;
 /**
  * This class will create files.txt with the personal information of the users
  * possible operation:
  * 1 --> add(Person)
- * 2 --> doesUserExit(Person)
+ * 2 --> doesUserExist(Person)
  * 3 --> removeUser(Person)  
  * @author LaundryApp
  */
 public class DatabaseManager {
-	
-	
-	
 	/**
 	 * saves space in disk for a Person object 
 	 * @param person object being added to the database
 	 * @return whether was able to create and save the Person on disk
 	 */
-	public boolean add(Person person) {
-		return true;
+	
+	//Adds a person to the database directory and then writes their info into a their personal file.
+	public void add(Person person) {
+		File file = new File(C:\Users\Sheldon\Documents\Java Projects\LaundryApp\src\database,
+				person.getName()+".txt");
+		PrintWriter output = new PrintWriter(person_info);
+		output.println("Name: " + person.getname());
+		output.println("PhoneNumber: " + String.valueOf(person.getPhoneNumeber()));
+		output.println("Username " + person.getUsername);
+		output.println("Password: " + person.getPassword());
+		output.close()
 	}
+	//Checks if the Persons's file exists and can be accessed.
+	public boolean doesUserExist(Person person){
+		if((person.getName()+".txt").exists()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public void remove(Person person) {
+		if((person.getName()+".txt").exists()) {
+			person.getName()+".txt".delete();
+		}
+		else {
+			return false;
+		} 
+
+}
 	
 }
--- src/laundrytimesheet/Person.java
+++ src/laundrytimesheet/Person.java
@@ -3,20 +3,31 @@ package laundrytimesheet;
 public class Person {
 		private String name;
 		private String password;
+		private String username;
 		private int phoneNumber;
 		
-		public String getName() {
-			return name;
-		}
+		//Sets and returns name.
 		public void setName(String name) {
 			this.name = name;
 		}
+		public String getName() {
+			return name;
+		}
+		//Sets and returns username.
+		public void setUsername(String password) {
+			this.password = password;
+		}
+		public String getUsername() {
+			return password;
+		}
+		//Sets and returns password.
 		public String getPassword() {
 			return password;
 		}
 		public void setPassword(String password) {
 			this.password = password;
 		}
+		//Sets and returns phone number.
 		public int getPhoneNumber() {
 			return phoneNumber;
 		}
