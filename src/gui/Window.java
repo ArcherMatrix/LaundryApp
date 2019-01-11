dc89e02656f6a385fa01d4d0107166cfef01624a
moisesmrp2009 <33405821+moisesmrp2009@users.noreply.github.com>
2019-01-11 18:41:23 -0500

    (feature) Creating an entry point to the program
    Window will serve as entry point and creating a custum Window where
    everything will be executed

---
+++ src/gui/Window.java
@@ -0,0 +1,25 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ *  This class is the entry point to the program
+ *  this class just creates a custom window for the program to run in window 
+ * @author Moises
+ */
+public class Window {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame window = new JFrame();
+                       window.setLayout(null);
+                       window.setSize(new Dimension(1000,800));
+                       //window.add();
+                       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                       window.setVisible(true);
+            }
+        });
+    }
+}
