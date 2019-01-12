23ced39785ab74090c475981469f1a01c6fb3eb7
moisesmrp2009 <33405821+moisesmrp2009@users.noreply.github.com>
2019-01-12 14:58:01 -0500

    (fix) little bug in CentralPanel

--- src/gui/CentralPanel.java
+++ src/gui/CentralPanel.java
@@ -1,9 +1,10 @@
 package gui;
 
-import com.sun.glass.events.KeyEvent;
+
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
